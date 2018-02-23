ad9232d040cd42cde52fbd7233a9efe965980fad
Jim to <jim@bluepandalabs.com>
2018-02-23 12:40:56 -0800

    table updates, removed dummy variable

--- public/js/home.js
+++ public/js/home.js
@@ -4,27 +4,3 @@ updateUrl = function () {
 		document.getElementById("room").href = window.location.origin + "/session/" + data.session;
 	})
 }
-
-
-var dummylinks = [
-	{
-		room: 1,
-		url: 'https://dev2.notarycam.com/session/2_MX40NjA2NTk1Mn5-MTUxOTM0MTIzNTA1OX5JeUptZ3Mvc1IwVjRjQU1BaEJ1dGtNUTB-UH4',
-	},
-	{
-		room: 2,
-		url: 'https://dev2.notarycam.com/session/2_MX40NjA2NTk1Mn5-MTUxOTM0MTIzNTA1OX5JeUptZ3Mvc1IwVjRjQU1BaEJ1dGtNUTB-UH4',
-	},
-	{
-		room: 3,
-		url: 'https://dev2.notarycam.com/session/2_MX40NjA2NTk1Mn5-MTUxOTM0MTIzNTA1OX5JeUptZ3Mvc1IwVjRjQU1BaEJ1dGtNUTB-UH4',
-	},
-	{
-		room: 4,
-		url: 'https://dev2.notarycam.com/session/2_MX40NjA2NTk1Mn5-MTUxOTM0MTIzNTA1OX5JeUptZ3Mvc1IwVjRjQU1BaEJ1dGtNUTB-UH4',
-	},
-	{
-		room: 5,
-		url: 'https://dev2.notarycam.com/session/2_MX40NjA2NTk1Mn5-MTUxOTM0MTIzNTA1OX5JeUptZ3Mvc1IwVjRjQU1BaEJ1dGtNUTB-UH4',
-	}
-]
